c82b7741cad7c9cfca9c2454af37f38419700d3a
GGhezali <gregory.ghezali@hotmail.fr>
2025-03-11 16:51:31 +0100

    script index modifié

--- index-script.js
+++ index-script.js
@@ -1,6 +1,21 @@
 document.querySelector(".search").addEventListener("click", function () {
-    console.log("click ok");
-    if (document.querySelector(".departure").value === "" || document.querySelector(".arrival").value === "" ) {
-        document.querySelector("#placeholder-train").computedStyleMap.display = "none";
-    }
-})
+  console.log("click");
+
+  const departure = document.querySelector(".departure").value;
+
+  const arrival = document.querySelector(".arrival").value;
+
+  const date = document.querySelector(".calendar").value;
+
+  console.log(date, departure, arrival);
+
+  fetch("http://localhost:3000/trajets", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ departure, arrival, date }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+});
