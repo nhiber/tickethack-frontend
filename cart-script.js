59099660497c5e575e29d88d06594143d3f6ea47
GGhezali <gregory.ghezali@hotmail.fr>
2025-03-12 13:18:35 +0100

    "new commit"

--- cart-script.js
+++ cart-script.js
@@ -1,21 +1,20 @@
 fetch("http://localhost:3000/chariots")
     .then((response) => response.json())
     .then((data) => {
-
+        console.log("data", data.data);
         if (data) {
-            document.querySelector("").style.display = "none";
+            //document.querySelector("").style.display = "none";
             let i = 0;
-            for (let trip of data) {
+            for (let trip of data.data) {
               document.querySelector(
                 ".carts"
               ).innerHTML += `<div class="cart-row">
                                 <p> <span id="departure-train-${i}">${trip.departure}</span> > <span id="arrival-train-${i}">${trip.arrival}</span></p> 
-                                <p id="time-train-${i}">${trip.time}</p>
+                                <p id="time-train-${i}">${trip.hour}:${trip.minute}</p>
                                 <p id="price-train-${i}">${trip.price}€</p>
-                                <button id="${i}" class="book-train" type="button">Book</button>
+                                <button id="${i}" class="purchase-train" type="button">Purchase</button>
                             </div>`;
               i++;
             }
-
-
+    }
     });
